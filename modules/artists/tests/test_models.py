9818002e3ca06c9aa528db4e04cc288ab1f57d20
C0l0red <blaizepaschal@gmail.com>
2021-09-05 02:16:00 +0100

    Fixes typo

--- modules/artists/tests/test_models.py
+++ modules/artists/tests/test_models.py
@@ -1,4 +1,4 @@
-from rest_framework.tests import APITestCase
+from rest_framework.test import APITestCase
 
 class TestArtistModel(APITestCase):
 
@@ -7,4 +7,3 @@ class TestArtistModel(APITestCase):
 
     def test_artist_model_saves_correctly(self):
         pass
-    
